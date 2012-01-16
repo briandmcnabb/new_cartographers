92765e81f38099936f7c895273ad16fb2526f2c0
Brian McNabb <brian.d.mcnabb@gmail.com>
2012-01-16 12:39:57 -0500

    Update config/application.rb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ NewCartographers::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  #config.action_controller.asset_host = ENV['RACKSPACE_CDN_HOST']
+  config.action_controller.asset_host = ENV['RACKSPACE_CDN_HOST']
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
